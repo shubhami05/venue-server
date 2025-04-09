d667b680a68a29714cf9d312aa9dee3b0e4e72af
shubham italiya <shubhamitaliya05@gmail.com>
2025-04-09 22:12:45 +0530

    Add root route to serve 'Hello World' response

--- index.js
+++ index.js
@@ -60,7 +60,9 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.get('/api/test-cors', (req, res) => {
   res.json({ message: 'CORS is working!' });
 });
-
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/owner",ownerRouter);
