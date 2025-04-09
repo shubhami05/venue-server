9dbd3d859582b31206ac6fe3eca30cf9f1628855
shubham italiya <shubhamitaliya05@gmail.com>
2025-04-09 23:26:18 +0530

    Update cookie settings in auth.controller.js to disable secure flag for non-production environments, improving local development experience.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,7 +16,7 @@ const generateToken = (user) => {
 const setCookie = (res, token) => {
     res.cookie("auth_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
         sameSite: "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
